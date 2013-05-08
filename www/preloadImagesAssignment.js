6f031afe35a63fc3055ad5c61d04ef2d5af3e03c
Dimitri Roche <dimroc@gmail.com>
2013-05-08 07:38:31 -0400

    Add in presentation assignment to preload images

---
+++ www/preloadImagesAssignment.js
@@ -0,0 +1,17 @@
+var imagesToPreload = [
+  "http://www.mranonymous.net/musicblog/wp-content/uploads/2009/02/jamaica-travel-tourism2.jpg",
+  "http://www.jamaica-konsulat.dk/images/jamaica1.jpg",
+  "http://www.hiltoncaribbean.com/wp-content/uploads/2012/08/jamaica-hotels.jpg",
+  "http://www.cntraveler.com/hotels/caribbean/jamaica/the-caves-negril-negril-jamaica/_jcr_content/par/cn_contentwell/par-main/cn_colctrl/par-col1/cn_features_containe/cn_manual_feature_0/cn_image_0.size.the-caves-negril-negril-jamaica-102019-1.jpg",
+  "http://ds-lands.com/data_images/countries/jamaica/jamaica-08.jpg",
+  "http://www.lib.utexas.edu/maps/americas/jamaica_rel_2002.jpg",
+  "http://jablogz.com/wp-content/uploads/2012/09/jamaica1.jpg",
+  "http://www.wallpapers-place.com/images/wallpapers/jamaica-hd-wallpaper-2560x1600.jpeg",
+  "http://2.bp.blogspot.com/-d54wxrghBK4/UVkhJOu4hcI/AAAAAAAAA5w/0coxed3BEX8/s1600/Jamaica+-+made+in.jpg"
+];
+
+
+var preloadImages = function() {
+  // Preload all images and only after alert the user.
+  // Use the html Image object: http://www.w3schools.com/jsref/dom_obj_image.asp
+};
